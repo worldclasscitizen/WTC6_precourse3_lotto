c2d1ff52a6f77f1ca79902ad6a491b6b69ca1ee2
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-13 06:30:16 +0900

    feat: 중복 로또 번호에 대한 예외 메시지 IS_DUPLICATED 추가

--- src/main/java/lotto/util/ErrorMessage.java
+++ src/main/java/lotto/util/ErrorMessage.java
@@ -2,9 +2,10 @@ package lotto.util;
 
 public class ErrorMessage {
     public static final String NEW_LINE = "\n";
-    public static final String ERROR_PREFIX = "[ERROR]";
-    public static final String NUMBER_FORMAT_ERROR_MESSAGE = ERROR_PREFIX + "입력값은 숫자여야 합니다." + NEW_LINE;
-    public static final String INVALID_UNIT = ERROR_PREFIX + "1000원 단위로 입력해 주세요." + NEW_LINE;
-    public static final String INVALID_NUMBER_RANGE = ERROR_PREFIX + "1~45 사이의 숫자여야 합니다." + NEW_LINE;
-    public static final String INVALID_LIST_SIZE = ERROR_PREFIX + "뭔가 이상합니다. 두 개의 세트를 합쳤는데 담긴 숫자가 6개도 안 됩니다." + NEW_LINE;
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String NUMBER_FORMAT_ERROR_MESSAGE = ERROR_PREFIX + "입력값은 숫자여야 합니다. " + NEW_LINE;
+    public static final String INVALID_UNIT = ERROR_PREFIX + "1000원 단위로 입력해 주세요. " + NEW_LINE;
+    public static final String INVALID_NUMBER_RANGE = ERROR_PREFIX + "1~45 사이의 숫자여야 합니다. " + NEW_LINE;
+    public static final String INVALID_LIST_SIZE = ERROR_PREFIX + "뭔가 이상합니다. 두 개의 세트를 합쳤는데 담긴 숫자가 6개도 안 됩니다. " + NEW_LINE;
+    public static final String IS_DUPLICATED = ERROR_PREFIX + "로또 번호에 중복이 있습니다. " + NEW_LINE;
 }
