436199c170cadfae0ffdf8ac9b815f33a7e6f69b
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-13 05:26:45 +0900

    feat: 사용자에게 보여줄 메시지를 관리하는 LottoMessage 클래스 추가

---
+++ src/main/java/lotto/util/LottoMessage.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+public class LottoMessage {
+    public static final String NEW_LINE = "\n";
+    public static final String GAME_START_MESSAGE = "구입금액을 입력해 주세요." + NEW_LINE;
+    public static final String LOTTO_PURCHASED_MESSAGE = "%d개를 구매했습니다." + NEW_LINE;
+
+    public static final String LOTTO_NUMBER_INPUT_MESSAGE = "당첨 번호를 입력해 주세요." + NEW_LINE;
+    public static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요." + NEW_LINE;
+
+    public static final String RESULT_MESSAGE = "당첨 통계" + NEW_LINE;
+    public static final String DIVIDER = "---" + NEW_LINE;
+    public static final String FIFTH_GRADE_MESSAGE = "3개 일치 (5,000원) - %d개" + NEW_LINE;
+    public static final String FOURTH_GRADE_MESSAGE = "4개 일치 (50,000원) - %d개" + NEW_LINE;
+    public static final String THIRD_GRADE_MESSAGE = "5개 일치 (1,500,000원) - %d개" + NEW_LINE;
+    public static final String SECOND_GRADE_MESSAGE = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개" + NEW_LINE;
+    public static final String FIRST_GRADE_MESSAGE = "6개 일치 (2,000,000,000원) - %d개" + NEW_LINE;
+    public static final String INTEREST_MESSAGE = "총 수익률은 %.1f%%입니다." + NEW_LINE;
+}
