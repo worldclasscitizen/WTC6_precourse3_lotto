7d19d4ba5fd94d24d83b6e833d1bb0fc3fae22cf
Jinwoo Choi <jk62362@mensakorea.org>
2024-10-13 05:21:44 +0900

    feat: 상금과 관련된 상수, 변수를 추가한 Constants 클래스 구현

---
+++ src/main/java/lotto/game/Constants.java
@@ -0,0 +1,12 @@
+package lotto.game;
+
+public class Constants {
+    public static final int FIFTH_PRIZE = 5000;
+    public static final int FOURTH_PRIZE = 50000;
+    public static final int THIRD_PRIZE = 1500000;
+    public static final int SECOND_PRIZE = 30000000;
+    public static final int FIRST_PRIZE = 2000000000;
+    public static int fifth, fourth, third, second, first;
+    public static float interest;
+
+}
